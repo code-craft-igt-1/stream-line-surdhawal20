d4aeac18bb16204f192f54766122b14313b20843
Surabhi Jain <Surabhi.Jain@philips.com>
2024-09-27 00:45:55 +0530

    streamLine

--- testSender.cpp
+++ testSender.cpp
@@ -1,16 +1,21 @@
 #include <gtest/gtest.h>
 #include "./sender.h"
+#include "Build/_deps/googletest-src/googlemock/include/gmock/gmock-matchers.h"
 
 class SenderTest : public ::testing::Test {
  protected:
-    void SetUp() override {
-    }
-
-    void TearDown() override {
-    }
-
-    TEST_F(SenderTest, GenerateRandomReadings) {
-    Sender generator;
-    generator.generateAndSendReadings(100);
-}
+    Sender sender;
 };
+
+TEST_F(SenderTest, GenerateRandomReadings) {
+    sender.generateAndSendReadings(100);
+}
+
+TEST_F(SenderTest, OutputFormattingConsole) {
+    testing::internal::CaptureStdout();
+    sender.generateAndSendReadings(1);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_THAT(output, ::testing::Contains("Temperature:"));
+    EXPECT_THAT(output, ::testing::Contains("Pulse Rate:"));
+    EXPECT_THAT(output, ::testing::Contains("SPO2:"));
+}
